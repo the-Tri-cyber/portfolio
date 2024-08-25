0265c3dd8a1bc0af9cc1f3cbb963d11c6e4513a7
Tri <trifauzi000@gmail.com>
2024-08-25 16:40:56 +0700

    portfolio-version-1.0.0

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: 'rgb(18, 172, 219)',
+        secondary: '#64748b',
+        dark: '#0f172a',
+      },
+      screens: {
+        'xl': '1420px',
+      },
+    },
+  },
+  plugins: [],
+}
+
